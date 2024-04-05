d6fcb958951fa1a31e6c5fe1970c507f18cbff9b
João Victor B. Azulay <jvazulay@outlook.com>
2024-04-05 18:20:13 -0300

    tela de login feita

--- Site/script.js
+++ Site/script.js
@@ -5,13 +5,35 @@ function initMap() {
         zoom: 12,
         center: myLatLng
     };
-
+    
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Meu Marcador'
+    map.addListener("click", function(event) {
+        var latitude = event.latLng.lat();
+        var longitude = event.latLng.lng();
+
+        buscarNomeLocal(latitude, longitude);
+    });
+}
+
+function buscarNomeLocal(latitude, longitude) {
+    var geocoder = new google.maps.Geocoder();
+    var latlng = { lat: latitude, lng: longitude };
+
+    geocoder.geocode({ 'location': latlng }, function(results, status) {
+        if (status === 'OK') {
+            if (results[0]) {
+                // Obtém o nome do local a partir dos resultados da geocodificação reversa
+                var nomeLocal = results[0].formatted_address;
+                
+                // Atualiza o texto na página com o nome do local
+                document.getElementById('areaSelecionada').innerText = nomeLocal;
+            } else {
+                console.log('Nenhum resultado encontrado');
+            }
+        } else {
+            console.log('Geocoder falhou devido a: ' + status);
+        }
     });
 }
 
@@ -24,4 +46,3 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.remove('active');
 }
-key = AIzaSyA_NMGLIkSTXvTFV0TGwYJfmo8iAPMPQEU
